1e586a6c9efce2ef48bb9ef11c02bfec1d31fc2f
appletree1218 <46469836+appletree1218@users.noreply.github.com>
2020-07-08 09:40:20 +0800

    add-update mybatis test

--- src/main/java/online/chenke/sourcecode/mybatis/MyBatisTest.java
+++ src/main/java/online/chenke/sourcecode/mybatis/MyBatisTest.java
@@ -1,6 +1,6 @@
 package online.chenke.sourcecode.mybatis;
 
-import online.chenke.sourcecode.mybatis.mapper.ScaleRecordMapper;
+import online.chenke.sourcecode.mybatis.mapper.RecordMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -13,8 +13,8 @@ public class MyBatisTest {
         InputStream is = MyBatisTest.class.getResourceAsStream("/mybatis-config.xml");
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
         SqlSession sqlSession = factory.openSession();
-        ScaleRecordMapper mapper = sqlSession.getMapper(ScaleRecordMapper.class);
-        Map map = mapper.selectLastRecord("6675671067190624256");
+        RecordMapper mapper = sqlSession.getMapper(RecordMapper.class);
+        Map map = mapper.selectRecord("6675671067190624256");
         System.out.println(map);
         sqlSession.close();
     }
