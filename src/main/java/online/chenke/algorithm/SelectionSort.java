7f52e5e01cb138693f38c64589b127048a5ffbfe
appletree1218 <46469836+appletree1218@users.noreply.github.com>
2020-08-05 15:02:28 +0800

    add-add SelectionSort

--- src/main/java/online/chenke/algorithm/SelectionSort.java
+++ src/main/java/online/chenke/algorithm/SelectionSort.java
@@ -1,7 +1,33 @@
 package online.chenke.algorithm;
 
-public class SelectionSort {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+/**
+ * 选择排序 从0开始遍历，拿出一个元素和后面的进行比较， 如果不是最小，记录下最小元素的索引，继续比较，走完一遍循环后将最小的那个元素和最外层的那个元素进行交换
+ */
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 3, 2, 1, 3, 4, 5, 6, 9, 7};
+        int len = array.length;
+        int minIndex, temp;
+
+        for (int i = 0; i < len; i++) {
+            minIndex = i;
+
+            for (int j = i + 1; j < len; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+
+            if (i != minIndex) {
+                temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
     }
 }
