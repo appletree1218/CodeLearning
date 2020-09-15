6303672710d9bb1bbc1f641f0ffb505520f2b741
appletree1218 <46469836+appletree1218@users.noreply.github.com>
2020-09-15 09:38:56 +0800

    add-修改算法

--- src/main/java/online/chenke/algorithm/BubbleSort.java
+++ src/main/java/online/chenke/algorithm/BubbleSort.java
@@ -6,15 +6,16 @@ import java.util.Arrays;
  * 冒泡排序, 升序排序
  */
 public class BubbleSort {
-    public static void main(String[] args) {
-        int[] array = new int[]{1,3,2,4,5,2,1,2,3};
 
-        for (int i=0;i<array.length; i++){
-            for (int j=0; j < array.length-1-i;j++){
-                if(array[j]>array[j+1]){
-                    array[j] = array[j]^array[j+1];
-                    array[j+1] = array[j]^array[j+1];
-                    array[j] = array[j]^array[j+1];
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 3, 2, 4, 5, 2, 1, 2, 3};
+
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    array[j] = array[j] ^ array[j + 1];
+                    array[j + 1] = array[j] ^ array[j + 1];
+                    array[j] = array[j] ^ array[j + 1];
                 }
             }
         }
