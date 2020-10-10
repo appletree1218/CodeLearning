fa45041688fa49ecda39732e09bc1cd3395bc3d9
appletree1218 <46469836+appletree1218@users.noreply.github.com>
2020-10-10 14:40:00 +0800

    add-添加注释

--- src/main/java/online/chenke/algorithm/InsertSort.java
+++ src/main/java/online/chenke/algorithm/InsertSort.java
@@ -4,24 +4,25 @@ import java.util.Arrays;
 
 /**
  * 插入排序
+ * 选一个元素，和前面的所有元素进行比较，如果小于前面的元素，就依次向后面移动，前面没有比当前选中元素大的了就停止，然后把当前元素填到那个位置上
  */
 public class InsertSort {
     public static void main(String[] args) {
-        int[] array = new int[]{4,3,5,3,1,1,2,4,8,7,5};
+        int[] array = new int[]{4, 3, 5, 3, 1, 1, 2, 4, 8, 7, 5};
 
-        for (int i = 1;i < array.length;i++){
-            int j = i-1;
+        for (int i = 1; i < array.length; i++) {
+            int j = i - 1;
             int temp = array[i];
 
-            for(; j>=0;j--){
-                if(temp<array[j]){
-                    array[j+1] = array[j];
-                }else{
+            for (; j >= 0; j--) {
+                if (temp < array[j]) {
+                    array[j + 1] = array[j];
+                } else {
                     break;
                 }
             }
 
-            array[j+1] = temp;
+            array[j + 1] = temp;
         }
 
         System.out.println(Arrays.toString(array));
